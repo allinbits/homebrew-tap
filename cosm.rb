4b62efe9bdbeb0b64c96303d9ad1970ff860f084
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-15 23:02:48 +0500

    Brew formula update for cosm version v0.0.14

--- cosm.rb
+++ cosm.rb
@@ -2,12 +2,12 @@
 class Cosm < Formula
   desc "CLI tool for developing Cosmos SDK apps."
   homepage "https://github.com/allinbits/cosm"
-  version "0.0.13"
+  version "0.0.14"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/allinbits/cosm/releases/download/v0.0.13/cosm_0.0.13_Darwin_x86_64.tar.gz"
-    sha256 "92668ab83e520549d98d0067d2f1315b18c8e9dffbacc0b2aab3097c28c80866"
+    url "https://github.com/allinbits/cosm/releases/download/v0.0.14/cosm_0.0.14_Darwin_x86_64.tar.gz"
+    sha256 "1b8411ad0f97f8892af019a8b937b5a4dbbde44583ad8f0da1ab2ededae57b62"
   elsif OS.linux?
   end
 
